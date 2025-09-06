797a87d42adf0bca229ba9067e87733528b5a762
ValipiReddy-web <72799893+ValipiReddy-web@users.noreply.github.com>
2025-09-06 16:04:11 +0530

    Update main.tf

--- environments/main/main.tf
+++ environments/main/main.tf
@@ -31,6 +31,7 @@ module "ec2_instance" {
   instance_type = var.instance_type
   subnet_id     = var.subnet_id
   key_name      = var.key_name
+  availability_zones = var.availability_zones
 }
 
 # S3 Module
