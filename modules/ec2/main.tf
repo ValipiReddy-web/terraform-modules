5ea7a4ab21718fa23166a6b4ffcf0b32ae1dbb63
ValipiReddy-web <72799893+ValipiReddy-web@users.noreply.github.com>
2025-09-06 16:21:50 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -4,10 +4,14 @@ resource "aws_instance" "this" {
   subnet_id     = var.subnet_id
   key_name      = var.key_name
 
+  # Pick the first AZ from the list
+  availability_zone = element(var.availability_zones, 0)
+
   tags = {
     Name = var.instance_name
   }
 }
+
 
 variable "ami_id" {}
 variable "instance_type" {}
